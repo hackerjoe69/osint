1c54776966f9aeade009adf33ffadbd1f1f076cd
joe <joe@kali.kali.org>
2025-03-28 11:01:09 +0100

    location tracking with phone no.

---
+++ location.py
@@ -0,0 +1,30 @@
+import phonenumbers
+from myphone import number
+import folium
+
+from phonenumbers import geocoder
+
+pepnumber = phonenumbers.parse(number)
+location = geocoder.description_for_number(pepnumber, "en")
+print(location)
+
+from phonenumbers import carrier
+service_provider = phonenumbers.parse(number)
+print(carrier.name_for_number(service_provider, "en"))
+
+from opencage.geocoder import OpenCageGeocode
+
+key = "bc735f48e6dc4c69a9c51400fca050e3"
+
+geocoder = OpenCageGeocode(key)
+query = str(location)
+results = geocoder.geocode(query)
+print(results)
+
+lat = results[0]['geometry']['lat']
+lng = results[0]['geometry']['lng']
+print(lat, lng)
+
+myMap = folium.Map(location=[lat, lng], zoom_start=9)
+folium.Marker([lat, lng], popup=location).add_to((myMap))
+myMap.save("mylocation.html")
